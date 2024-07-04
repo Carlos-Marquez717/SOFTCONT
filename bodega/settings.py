8b3647471cd44b1e5e13e12300047237bff44a8f
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-07-04 18:59:02 -0400

    database

--- bodega/settings.py
+++ bodega/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
+import dj_database_url
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 from telnetlib import LOGOUT
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -178,11 +183,10 @@ WSGI_APPLICATION = 'bodega.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
