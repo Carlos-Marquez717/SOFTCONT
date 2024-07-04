34fa1d100994191acc5d497af821d69c1315c125
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-07-04 18:44:23 -0400

    csrf

--- bodega/settings.py
+++ bodega/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','web-production-b3ad.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS=['http://*','https://web-production-b3ad.up.railway.app']
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
