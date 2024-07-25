ca4d2335e574165d7091201a1eeeef74d5b122f1
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-07-25 16:29:43 -0400

    1003%

--- bodega/settings.py
+++ bodega/settings.py
@@ -181,12 +181,7 @@ WSGI_APPLICATION = 'bodega.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
