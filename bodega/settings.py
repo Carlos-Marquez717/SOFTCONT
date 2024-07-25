35847e5e892d5601952582734b35abe838637cdd
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-07-25 16:32:20 -0400

    1004%

--- bodega/settings.py
+++ bodega/settings.py
@@ -184,7 +184,7 @@ WSGI_APPLICATION = 'bodega.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
