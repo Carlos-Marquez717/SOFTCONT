3e41da33c9beabb5ae5c531efd450715d4abbb95
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-07-28 17:58:14 -0400

    libertad8

--- bodega/settings.py
+++ bodega/settings.py
@@ -236,21 +236,3 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'SOFTCON2/logs/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
