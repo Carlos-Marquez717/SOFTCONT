8102c5a8f7279beeb883f51d6f1f05c68b2766a6
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2024-08-10 13:55:49 -0400

    logo admin

--- bodega/settings.py
+++ bodega/settings.py
@@ -68,7 +68,7 @@ JAZZMIN_SETTINGS = {
     "site_title": "SOFTCON",
     "site_header": "your_site_header",
     "site_brand": "SOFTCON",
-    "site_icon": "images/favicon.png",
+    "site_icon": "images/FONDO.png",
     # Add your own branding here
     "site_logo": None,
     "welcome_sign": "BIENVENIDO",
