e20a8bdb039b4d196211b1117109c5e6c0365f0a
Carlos-Marquez717 <ing.marquezcarlos@gmail.com>
2025-07-09 18:36:16 -0400

    despliegue17

--- bodega/settings.py
+++ bodega/settings.py
@@ -20,7 +20,10 @@ SECRET_KEY = 'django-insecure-=c*xc^a^%l2l%0*m=66o*bxeo*(m2w_n+mb2d@tn8#^awgtpwa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://softcont-production.up.railway.app']
+
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+
 
 
 # Edit the following line and place your railway URL, and your custom URL in the array.
